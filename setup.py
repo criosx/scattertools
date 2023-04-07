325c322c4c6093a235740db37f2adc28b8faf8d8
Frank Heinrich <mail@frank-heinrich.net>
2023-04-06 23:33:33 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setuptools.setup(
     version='0.9',
     packages=["scattertools", "scattertools/ml", "scattertools/support", "scattertools/infotheory"],
     url='https://github.com/criosx/scattertools',
-    license='GNU GENERAL PUBLIC LICENSE',
+    license='MIT Permissive License',
     author='Frank Heinrich',
     author_email='mail@frank-heinrich.net',
     description='Data analysis, experimental optimization, and machine learning for neutron and x-ray scattering.'
