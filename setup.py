7990e0741501e591028f383fcb43624d3ce558cb
criosx <mail@frank-heinrich.net>
2023-04-06 21:55:10 -0400

    Copy files from molgroups repository.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='scattertools',
+    version='0.9',
+    packages=['ml', 'infotheory'],
+    package_dir={'': 'scattertools'},
+    url='',
+    license='GNU GENERAL PUBLIC LICENSE',
+    author='Frank Heinrich',
+    author_email='mail@frank-heinrich.net',
+    description='Data analysis, experimental optimization, and machine learning for neutron and x-ray scattering.'
+)
